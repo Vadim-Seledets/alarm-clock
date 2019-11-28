5f55169c1bc6ed210edf5f41610f0821ba10e210
Raman Bortnik <raman_bortnik@epam.com>
2019-11-29 02:32:02 +0300

    Fix visibility clock controller

--- Source/ClockController.vhd
+++ Source/ClockController.vhd
@@ -48,13 +48,13 @@ begin
 		end if;
 	end process;
 	
-	SetTimeEnable: process (SetTime)
+	SetTime: process (SetTime)
 	begin
 		if rising_edge(SetTime)then
 			CurrentMode <= SettingTimer;
-			Seconds <= TimeIn(7) & TimeIn(6) & TimeIn(5) & TimeIn(4) & TimeIn(3) & TimeIn(2) & TimeIn(1) & TimeIn(0);
-			Minutes <= TimeIn(15) & TimeIn(14) & TimeIn(13) & TimeIn(12) & TimeIn(11) & TimeIn(10) & TimeIn(9) & TimeIn(8);
-			Hours <= TimeIn(23) & TimeIn(22) & TimeIn(21) & TimeIn(20) & TimeIn(19) & TimeIn(18) & TimeIn(17) & TimeIn(16);
+			Seconds <= TimeIn(7 downto 0);
+			Minutes <= TimeIn(15 downto 8);
+			Hours <= TimeIn(23 downto 16);
 			CurrentMode <= RunningTimer;
 		end if;
 	end process;
