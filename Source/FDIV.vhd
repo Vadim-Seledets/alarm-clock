932f3dac7f7bd8ec9dd43464d970361772ec8099
TheG-Man <Vadik446s@gmail.com>
2019-12-02 22:17:13 +0300

    Make FDIV be more flexible

--- Source/FDIV.vhd
+++ Source/FDIV.vhd
@@ -4,29 +4,32 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity FDIV is
-	port (
-		Threshold: std_logic_vector(31 downto 0);
-      CLK: in std_logic;
-		DividedCLK: out std_logic
-	);
+    generic (
+        size: integer
+    );
+    port (
+        Threshold: std_logic_vector(size - 1 downto 0);
+        CLK: in std_logic;
+        DividedCLK: out std_logic
+    );
 end FDIV;
 
-architecture behavioral of FDIV is	
-	signal Ticks: std_logic_vector(31 downto 0) := (others => '0');
-	signal Result: std_logic := '0';
+architecture behavioral of FDIV is    
+    signal s_ticks: std_logic_vector(size - 1 downto 0) := (others => '0');
+    signal s_result: std_logic := '0';
 begin
     Main: process (CLK)
     begin
         if rising_edge(CLK) then 
-            Ticks <= Ticks + 1;
-            if Ticks < Threshold then 
-                Result <= '0';
+            s_ticks <= s_ticks + 1;
+            if s_ticks < Threshold then 
+                s_result <= '0';
             else 
-                Result <= '1';
-                Ticks <= (others => '0');
+                s_result <= '1';
+                s_ticks <= (others => '0');
             end if;
         end if;
     end process;
 
-    DividedCLK <= Result;
+    DividedCLK <= s_result;
 end behavioral;
